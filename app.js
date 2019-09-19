74307a8c76796f9460887ef2ffb63dd2a110c372
Sam <sam@isthebestcoder.com>
2019-09-19 12:33:39 +0500

    PDF generation

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false});
+  const browser = await puppeteer.launch({ headless: true, defaultViewport: null});
   const page = await browser.newPage();
 
-  await page.goto('https://www8.miamidade.gov/Apps/PA/propertysearch/#/');
+  await page.goto('https://www8.miamidade.gov/Apps/PA/propertysearch/#/',{waitUntil: 'networkidle0'});
   //await page.screenshot({path: 'delta.png'});
   //await page.waitFor(3000);
 
@@ -12,13 +12,16 @@ const puppeteer = require('puppeteer');
   await page.focus('#search_box')
   await page.keyboard.type(address);
 
-  await page.evaluate(() => {
+  await page.evaluate(async () => {
 
     //document.getElementById('search_box').value = '1630 sw 13th ave';
     document.getElementById('search_submit').click();
-   
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    document.querySelectorAll("#results_list > div > div.results_record.ng-scope > div.record_folio.ng-binding > span")[0].click();
   
   });
+  await page.waitFor(5000);
+  await page.pdf({path: 'page.pdf'});
 
   //await browser.close();
 })();
